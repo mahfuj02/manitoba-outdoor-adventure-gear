e83c60065139d21f19f10cd8520041569da0f396
mahfuj02 <ahmed.mahfuj02@gmail.com>
2025-03-26 15:16:40 -0500

    [Config] Install Active Storage for image uploads

--- app/models/product.rb
+++ app/models/product.rb
@@ -15,6 +15,6 @@ class Product < ApplicationRecord
   
   # Define which associations can be searchable through Ransack
   def self.ransackable_associations(auth_object = nil)
-    ["category", "image_attachment", "image_blob"]
+    ["category", "image_attachment", "image_blob"] 
   end
 end
