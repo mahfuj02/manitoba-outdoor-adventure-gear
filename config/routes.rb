48c8ddf20acbd1357561f8720347bcb17e605b22
mahfuj02 <ahmed.mahfuj02@gmail.com>
2025-04-08 23:38:24 -0500

    implement views and controller for cart and cart items

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,13 @@ Rails.application.routes.draw do
   get 'about', to: 'about#index'
   get 'contact', to: 'contact#index'
 
+
+   # Cart routes
+   resource :cart, only: [:show]
+   post 'cart/add/:product_id', to: 'carts#add_item', as: 'add_to_cart'
+   patch 'cart/update/:id', to: 'carts#update_item', as: 'update_cart_item'
+   delete 'cart/remove/:id', to: 'carts#remove_item', as: 'remove_cart_item'
+
   root 'products#index'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
