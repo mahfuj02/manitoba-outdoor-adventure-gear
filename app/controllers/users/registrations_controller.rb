f948de1ccfb1f1666602151e4a6acabe9541f732
mahfuj02 <ahmed.mahfuj02@gmail.com>
2025-04-16 12:03:59 -0500

    added address in the user site for the orders

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,25 @@
 class Users::RegistrationsController < Devise::RegistrationsController
     before_action :configure_sign_up_params, only: [:create]
     before_action :configure_account_update_params, only: [:update]
+
+     # GET /resource/sign_up
+    def new
+      super do |resource|
+        @provinces = Province.order(:name)
+        resource.build_address if resource.address.nil?
+      end
+    end
+
+  # POST /resource
+    def create
+      super do |resource|
+        if resource.persisted? && params[:address].present?
+          address = resource.addresses.build(address_params)
+          address.is_default = true
+          address.save
+        end
+      end
+    end
   
     protected
   
@@ -11,5 +30,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   
     def configure_account_update_params
       devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone])
+    end
+
+    private
+
+    def address_params
+      params.require(:address).permit(:street, :city, :province_id, :postal_code, :country)
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
 
+  # Address management
+  resources :addresses do
+    member do
+      patch :set_default
+    end
+  end
+
   get "carts/show"
   get "carts/add_item"
   get "carts/update_item"
@@ -18,34 +25,28 @@ Rails.application.routes.draw do
   get 'about', to: 'about#index'
   get 'contact', to: 'contact#index'
 
-
-# config/routes.rb (cart routes)
+  # Cart routes
   get 'cart', to: 'carts#show', as: 'cart'
   post 'cart/add/:product_id', to: 'carts#add_item', as: 'add_to_cart'
-# or if you're using 'id' in the URL:
-# post 'cart/add/:id', to: 'carts#add_item', as: 'add_to_cart'
   patch 'cart/update/:id', to: 'carts#update_item', as: 'update_cart_item'
   delete 'cart/remove/:id', to: 'carts#remove_item', as: 'remove_cart_item'
-  root 'products#index'
+  
+  # Order routes
+  resources :orders, only: [:index, :show]
+  
+  # Checkout process
+  get 'checkout', to: 'checkout#index'
+  post 'checkout/address', to: 'checkout#create_address'
+  get 'checkout/review', to: 'checkout#review'
+  post 'checkout/complete', to: 'checkout#complete'
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # Health check
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/*
+  # PWA files
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-
-  resources :orders, only: [:index, :show]
-   # Checkout process
-   get 'checkout', to: 'checkout#index'
-   post 'checkout/address', to: 'checkout#create_address'
-   get 'checkout/review', to: 'checkout#review'
-   post 'checkout/complete', to: 'checkout#complete'
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Root path
+  root 'products#index'
 end
